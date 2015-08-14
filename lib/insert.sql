aa456722912d7684ea4697b4c6dd7b120478c06d
NancyKo <konancys@gmail.com>
2015-08-14 16:16:13 -0400

    Got it done with some lots of leads

---
+++ lib/insert.sql
@@ -0,0 +1,14 @@
+INSERT INTO bears(name, age, gender, color, temperament, alive) 
+VALUES 
+('Mr. Chocolate', 18, 'M', 'Brown', 'Friendly', 1),
+('Rowdy', 40, 'M', 'Red', 'Mean', 1),
+('Tabitha', 11, 'F', 'Brown', 'Friendly', 1),
+('Sergeant Brown', 14, 'M', 'Brown', 'Friendly', 1),
+('Melissa', 16, 'F', 'Yellow', 'Friendly', 1),
+('Grinch', 17, 'M', 'Brown', 'Friendly', 1),
+('Wendy', 19, 'F', 'Green', 'Friendly', 1),
+(NULL , 33, 'M', 'Brown', 'Friendly', 0);
+
+
+
+
